ccff3ddee125af7ef7b668e66c1f4f67056a14ec
tylermachado <3586466+tylermachado@users.noreply.github.com>
2019-10-07 16:29:06 -0400

    Revert "establish template for HOCR flyby"
    This reverts commit 66f165a54c9d3158ed327a4e6e0b09507883e1da.

--- app/js/plugins/boilerplate.js
+++ app/js/plugins/boilerplate.js
@@ -1,77 +1,35 @@
-mapboxgl.accessToken = 'pk.eyJ1IjoidG1hY2hhZG9udSIsImEiOiJjanVjdDFudDMwMDR4NGRtdGJ4NndiaW9pIn0.JS0ffUQym0L07752GAwMFg';
+var margin = {top:25, right:0, bottom:25, left:25};
 
-var map = new mapboxgl.Map({
-    container: 'map',
-    style: 'mapbox://styles/mapbox/satellite-v9',
-    center: [-71.129700, 42.363347],
-    zoom: 13,
-    bearing: 0,
-    pitch: 0
+// var colors = {
+//     bold: ["#d51e2d", "#52CFE5", "#385775", "#FFBF3D", "#6f2b6e", "#00CFB5"],
+//     pastel: ["#e59097", "#c0adcc", "#b3cadd", "#a3ceaf", "lavender", "aquamarine", "gold"],
+//     procon: ["#ce0201", "#a5d65a"],
+//     political: ["#D41B2C", "#006EB5"]
+// }
+
+var colors = {
+    bold: ["#d51e2d", "#52CFE5", "#385775", "#FFBF3D", "#6f2b6e", "#00CFB5"],
+    pastel: ["#e59097", "#c0adcc", "#b3cadd", "#a3ceaf", "lavender", "aquamarine", "gold"],
+    procon: ["#cc0000", "#808080", "#000"],
+    political: ["#D41B2C", "#006EB5"]
+}
+
+
+d3.json('/interactive/2018/10/bubble/data/aggregated.json')
+  .then(function(data) {
+
+  columnTemplate(data, "#column");
+
+  groupedColumnTemplate(data, "#groupedcolumn");
+
+  lineTemplate(data, "#line");
+
+  multiLineTemplate(data, "#multiline");
+
+    barTemplate(data, "#bar");
+
+   groupedbarTemplate(data, "#groupedbar");
+
+}).catch(function(error){
+   // handle error
 });
-
-var chapters = {
-    'start': {
-        center: [-71.129700, 42.363347],
-        zoom: 13,
-        bearing: 0,
-        pitch: 0
-    },
-    'river': {
-        center: [-71.117061, 42.361054],
-        zoom: 18,
-        bearing: 0,
-        pitch: 90,
-        speed: 0.5
-    },
-    'weeks': {
-        center: [-71.117651, 42.368204],
-        zoom: 16,
-        bearing: -60,
-        pitch: 60,
-        speed: 0.5
-    },
-    'eliot': {
-        center: [-71.133133, 42.372397],
-        zoom: 17,
-        bearing: 180,
-        pitch: 80,
-        speed: 0.5
-    },
-    'finish': {
-        center: [-71.135933, 42.367181],
-        zoom: 16,
-        bearing: 0,
-        pitch: 0,
-        speed: 0.5
-    }
-};
-
-// On every scroll event, check which element is on screen
-window.onscroll = function() {
-    var chapterNames = Object.keys(chapters);
-    for (var i = 0; i < chapterNames.length; i++) {
-        var chapterName = chapterNames[i];
-        if (isElementOnScreen(chapterName)) {
-            setActiveChapter(chapterName);
-            break;
-        }
-    }
-};
-
-var activeChapterName = 'baker';
-function setActiveChapter(chapterName) {
-    if (chapterName === activeChapterName) return;
-
-    map.flyTo(chapters[chapterName]);
-
-    document.getElementById(chapterName).setAttribute('class', 'active');
-    document.getElementById(activeChapterName).setAttribute('class', '');
-
-    activeChapterName = chapterName;
-}
-
-function isElementOnScreen(id) {
-    var element = document.getElementById(id);
-    var bounds = element.getBoundingClientRect();
-    return bounds.top < window.innerHeight && bounds.bottom > 0;
-}
